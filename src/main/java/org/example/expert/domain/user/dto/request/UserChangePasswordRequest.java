e019b1544bbeda69255f1c4794bf9ab79a14f0af
DESKTOP-HIO8OC0\User <uhk561@naver.com>
2025-12-04 04:20:35 +0900

    ♻ refactor: Lv.3 리팩토링완료
    불필요한 로직 제거 및  가독성 증가, 유효성 검증 추가

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,9 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
+
+    @Size(min = 8, message = "8글자 이상이여야합니다")
+    @Pattern(regexp = ".*[A-Z].*", message = "숫자와 대문자를 각각 1개이상 포함하여야합니다.")
     @NotBlank
     private String newPassword;
 }
